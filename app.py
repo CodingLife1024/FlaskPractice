f3ab25912822a5eeece4d5aaff8d6f7763f43cd1
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-04-08 15:56:40 +0530

    aallalallaal

--- app.py
+++ app.py
@@ -50,25 +50,57 @@ def login():
     else:
         return render_template('login.html')
 
-@app.route("/popular")
+@app.route("/popular", methods=['GET', 'POST'])
 def popular():
     return render_template('popular.html')
 
-@app.route("/timeline")
+@app.route("/timeline", methods=['GET', 'POST'])
 def timeline():
     return render_template('timeline.html')
 
-@app.route("/search")
+@app.route('/search', methods=['GET', 'POST'])
 def search():
+    if request.method == 'POST':
+        search_results = request.form['searchresults']
+        # do something with the search results
+        return f'Search results for "{search_results}"'
     return render_template('search.html')
 
-@app.route("/profile")
+@app.route("/profile", methods=['GET', 'POST'])
 def profile():
     return render_template('profile.html')
+
+@app.route("/profile/followers", methods=['GET', 'POST'])
+def profilefollowers():
+    return render_template("followers.html")
+
+@app.route("/profile/following", methods=['GET', 'POST'])
+def profilefollowing():
+    return render_template("following.html")
+
+@app.route("/profile/posts", methods=['GET', 'POST'])
+def profileposts():
+    return render_template("profileposts.html")
 
 @app.route("/create")
 def create():
     return "45345456"
+
+@app.route('/comments', methods=['GET', 'POST'])
+def comments():
+    return "commmmmm"
+
+@app.route('/comments/new', methods=['GET', 'POST'])
+def index():
+    if request.method == 'POST':
+        if request.form['submit_button'] == 'submit':
+            # do something with submitted data
+            return "Comment Submitted"
+        elif request.form['submit_button'] == 'discard':
+            # discard submitted data
+            return "Comment Discarded"
+    else:
+        return render_template('addComments.html')
 
 if __name__ == '__main__':
     app.run(debug=True)
