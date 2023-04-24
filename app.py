a0902f211df81e7efefdf6bfc77336646ef5263c
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-04-24 21:03:50 +0530

    irjvo4hih i3[6ih [6hi 53ho56yhu 3p6hubp u356bpu 3bpugbp6u bp3u5b35pu

--- app.py
+++ app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, url_for, flash, redirect, request, ses
 import sqlite3
 from werkzeug.exceptions import abort
 import mysql.connector
-from flask-bcrypt import Bcrypt
+from flask_bcrypt import Bcrypt
 
 def get_db_connection():
     conn = sqlite3.connect('database.db') 
