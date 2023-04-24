d9a69546d94bdc1dd1a0980d509363adf823af41
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-04-24 22:11:38 +0530

    uu3i4b34u

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ app.config['SECRET_KEY'] = '5791628bb0b13ce0c676dfde280ba245'
 app.config['MYSQL_HOST'] = 'localhost'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = ''
-app.config['MYSQL_DB'] = 'pythonlogin'
+app.config['MYSQL_DB'] = 'login'
 
 mysql = MySQL(app)
 
