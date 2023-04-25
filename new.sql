d8c700bb9ae0ff9d37500e4a03416d5d21fb4f14
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-04-25 16:11:58 +0530

    skrgehthb

--- new.sql
+++ new.sql
@@ -1,6 +1,6 @@
 DROP TABLE IF EXISTS users;
 DROP TABLE IF EXISTS posts;
-DROP TABLE IF EXISTS follows;
+DROP TABLE IF EXISTS comments;
 
 CREATE TABLE users (
     user_id INTEGER PRIMARY KEY AUTOINCREMENT,
@@ -17,4 +17,13 @@ CREATE TABLE posts(
     created DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
     pic LONGBLOB NOT NULL DEFAULT "static/unk.jpg",
     FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE comments(
+    comment_id INTEGER PRIMARY KEY AUTOINCREMENT,
+    user_id INTEGER NOT NULL,
+    post_id INTEGER NOT NULL,
+    content TEXT NOT NULL,
+    FOREIGN KEY (user_id) REFERENCES users(user_id),
+    FOREIGN KEY (post_id) REFERENCES posts(post_id)
 );
