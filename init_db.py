bfa5530f3aa4cbf31df3b3f6fea70f58e7b6f3c5
Riddhidipta Pal <riddhidipta2003@gmail.com>
2023-04-28 22:49:42 +0530

    iesjtvowh4uotbw4u

--- init_db.py
+++ init_db.py
@@ -1,4 +1,5 @@
 import sqlite3
+import csv
 
 connection = sqlite3.connect('database.db')
 
@@ -11,25 +12,11 @@ cur.execute("INSERT INTO users (username, pass_word) VALUES (?, ?)", ("Riddhi", 
 cur.execute("INSERT INTO users (username, pass_word) VALUES (?, ?)", ("Mrunal", "Mrunal"))
 cur.execute("INSERT INTO users (username, pass_word) VALUES (?, ?)", ("Kanu", "Kanu"))
 
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (1, "This is Riddhi's first post."))
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (1, "This is Riddhi's second post."))
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (1, "This is Riddhi's third post."))
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (1, "This is Riddhi's fourth post."))
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (1, "This is Riddhi's fifth post."))
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (1, "This is Riddhi's sixth post."))
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (1, "This is Riddhi's seventh post."))
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (1, "This is Riddhi's eighth post."))
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (1, "This is Riddhi's ninthpost."))
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (1, "This is Riddhi's tenth post."))
-cur.execute("INSERT INTO posts (user_id, content) VALUES (?, ?)", (3, "This is Kanishka's first post."))
-
-cur.execute("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", (1, 1, "Comment1"))
-cur.execute("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", (1, 1, "Comment2"))
-cur.execute("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", (1, 1, "Comment3"))
-cur.execute("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", (1, 1, "Comment4"))
-cur.execute("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", (1, 1, "Comment5"))
-cur.execute("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", (1, 1, "Comment6"))
-cur.execute("INSERT INTO comments (user_id, post_id, content) VALUES (?, ?, ?)", (1, 1, "Comment7"))
+with open('user.csv', 'r') as f:
+    reader = csv.reader(f)
+    next(reader)  # Skip header row
+    for row in reader:
+        cur.execute("INSERT INTO users (username, pass_word, bio) VALUES (?, ?, ?)", row)
 
 connection.commit()
 connection.close()
